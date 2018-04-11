4fdb4562d4b29b57b4e3ac7eb623527102e5a13f
Hopper II, Alan T <alan.hopper@one.verizon.com>
2018-04-11 08:12:05 -0400

    fix register bug, mutex for active state

---
+++ MCUNetworkNode.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Hopper II, Alan T on 4/5/18.
+//
+
+#include "MCUMeshNode.h"
+#include "RF24MeshNode.h"
+#include "ThreadNames.h"
+
+using namespace std;
+
+MCUMeshNode::MCUMeshNode(int id, shared_ptr<Ether> someEther) : ether(someEther), nodeID(id)
+{
+    setup();
+}
+
+void MCUMeshNode::start()
+{
+    running = true;
+    mcu = thread(&MCUMeshNode::loop, this);
+    mcu.detach();
+}
+
+void MCUMeshNode::stop()
+{
+    running = false;
+}
+
+
+void MCUMeshNode::loop()
+{
+    ThreadNames::setName(string("mc")+to_string(nodeID));
+    printf("%s mcu started\n",radio->rf24->LOGHDR);
+
+    mesh->begin();
+    this_thread::yield();
+
+    bool tx = false;
+
+    while (running)
+    {
+        mesh->update();
+
+        while (network->available())
+        {
+            char buffer[32];
+            RF24NetworkHeader hdr;
+            network->read(hdr,buffer,sizeof(buffer));
+            printf("%s from %o buffer=%s\n",radio->rf24->LOGHDR, hdr.from_node,buffer);
+        }
+        this_thread::yield();
+        this_thread::sleep_for(chrono::milliseconds(2));
+
+        //if (tx && mesh->mesh_address!=MESH_DEFAULT_ADDRESS)
+        if (tx)
+        {
+            RF24NetworkHeader header;
+            header.to_node = 00;
+            header.type = 75;
+            char msg[15] = "sendTo00";
+            network->write(header,msg,sizeof(msg));
+            tx=false;
+        }
+    }
+    printf("%s mcu stopped\n",radio->rf24->LOGHDR);
+}
+
+void MCUMeshNode::setup()
+{
+    radio = unique_ptr<RF24>(new RF24(9,10,new nRF24l01plus(nodeID,ether.get(),clock),clock));
+    network = unique_ptr<RF24Network>(new RF24Network(*radio,300,clock));
+    mesh = unique_ptr<RF24MeshNode>(new RF24MeshNode(*radio,*network,clock));
+    mesh->setNodeID(nodeID);
+}
---
+++ MCUNetworkNode.h
@@ -0,0 +1,38 @@
+//
+// Created by Hopper II, Alan T on 4/5/18.
+//
+
+#ifndef NRF_SIMULATOR_MESHMCU_H
+#define NRF_SIMULATOR_MESHMCU_H
+
+#include <thread>
+#include <memory>
+#include "RF24MeshNode.h"
+#include "MCUClock.h"
+
+class MCUMeshNode
+{
+    public:
+       MCUClock clock;
+
+    private:
+       std::unique_ptr<RF24> radio;
+       std::unique_ptr<RF24Network> network;
+       std::unique_ptr<RF24MeshNode> mesh;
+       std::shared_ptr<Ether> ether;
+
+       int nodeID = 0;
+       bool running = false;
+
+       std::thread mcu;
+
+    public:
+       MCUMeshNode(int id, std::shared_ptr<Ether> someEther);
+       void start();
+       void stop();
+    protected:
+       void setup();
+       void loop();
+};
+
+#endif //NRF_SIMULATOR_MESHMCU_H
